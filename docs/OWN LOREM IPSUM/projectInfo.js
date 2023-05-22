6bb7bdc3d5fda9336623904d80afe0ee0b856635
zyjaruWeb <128094313+zyjaruWeb@users.noreply.github.com>
2023-05-22 16:32:52 +0100

    Update projectInfo.js

--- docs/OWN LOREM IPSUM/projectInfo.js
+++ docs/OWN LOREM IPSUM/projectInfo.js
@@ -2,9 +2,9 @@ const ownLoremIpsum = {
         id: 1,
         title: "Alt Lorem Ipsum",
         category: "JavaScript",
-        img: "https://github.com/zyjaruWeb/portfolio/blob/main/OWN%20LOREM%20IPSUM/thumbnailLoremIpsum.PNG",
+        img: "./OWN LOREM IPSUM/thumbnailLoremIpsum.PNG",
         description: "This project displays up to 1000 paragraphs of alternative lorem ipsum text. It uses pre-generated 9 paragraphs, and randomises them at every click, displaying neccessary amount of paragraphs based on the number entered by the user. So convenient!",
-        link:"../OWN LOREM IPSUM/index.html",
+        link:"./OWN LOREM IPSUM/index.html",
 }
 
 export {ownLoremIpsum}
