8ec3cdff043391624301331f38192aaf2eaacb0f
zyjaruWeb <128094313+zyjaruWeb@users.noreply.github.com>
2023-05-22 02:04:58 +0100

    Update projectInfo.js

--- COLOR FLIPPER/projectInfo.js
+++ COLOR FLIPPER/projectInfo.js
@@ -4,7 +4,7 @@ const colorFlipper = {
         category: "JavaScript",
         img: "../COLOR FLIPPER/thumbnailColorFlipper.PNG",
         description: "This project aim is to display random colors and its professional reference. There are two types, simple and hex colors. Simple colors are saved in an array and randomly displayed at each click of a button. Hex colours offer more diversity by randomly displaying a color with reference out of a total 16777216 colors. Amazing!",
-        link:"/COLOR FLIPPER/simple.html",
+        link:"../COLOR FLIPPER/simple.html",
 }
 
 export {colorFlipper}
