6cfeb15f4bf40f4ddee3d666553a18f690d3bdf1
zyjaru <128094313+zyjaruWeb@users.noreply.github.com>
2023-06-19 20:39:54 +0100

    Add files via upload

--- docs/COLOR FLIPPER/projectInfo.js
+++ docs/COLOR FLIPPER/projectInfo.js
@@ -5,7 +5,7 @@ const colorFlipper = {
         img: "./images/thumbnailColorFlipper.PNG",
         description: "This is a simple JS/CSS/HTML frontend project whose main function is to randomly display colours of two different types. First, simple, randomly displays colours from an array of simple colours. The Hex, displays colours randomly selected from the total of 16,777,216 colours.",
         link:"./COLOR FLIPPER/simple.html",
-        githubLink:"https://github.com/zyjaruWeb/portfolio/tree/https://github.com/zyjaruWeb/portfolio/tree/b4f8220352898ae887a58a3f4f96d50c9cd7c3c1/docs/COLOR%20FLIPPER/docs/COLOR%20FLIPPER",
+        githubLink:"https://github.com/zyjaruWeb/portfolio/tree/372a04e355ead4c29935b6af9f151377bc1d9f4a/docs/COLOR%20FLIPPER",
 }
 
 export {colorFlipper}
