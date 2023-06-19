1a781199095cf10829b713287ac66877f4e7498b
zyjaru <128094313+zyjaruWeb@users.noreply.github.com>
2023-06-19 22:09:07 +0100

    Add files via upload

--- docs/OWN LOREM IPSUM/projectInfo.js
+++ docs/OWN LOREM IPSUM/projectInfo.js
@@ -5,7 +5,7 @@ const ownLoremIpsum = {
         img: "./images/thumbnailLoremIpsum.PNG",
         description: "This is a simple Frontend project which uses JS/CSS/HTML. It displays up to 1000 paragraphs of alternative lorem ipsum text. It uses pre-generated 9 paragraphs, and randomises them at every click, displaying neccessary amount of paragraphs based on the number entered by the user.",
         link:"./OWN LOREM IPSUM/index.html",
-        githubLink:"https://github.com/zyjaruWeb/portfolio/tree/372a04e355ead4c29935b6af9f151377bc1d9f4a/docs/OWN%20LOREM%20IPSUM",
+        githubLink:"https://github.com/zyjaruWeb/portfolio/tree/main/docs/OWN%20LOREM%20IPSUM",
 }
 
 export {ownLoremIpsum}
