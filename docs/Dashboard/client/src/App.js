595e120bda9439bcf37d8a81050f241c01721389
zyjaru <128094313+zyjaruWeb@users.noreply.github.com>
2023-07-26 00:30:01 +0100

    Add files via upload

---
+++ docs/Dashboard/client/src/App.js
@@ -0,0 +1,36 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
+import Products from "scenes/products";
+import Customers from "scenes/customers";
+import Transactions from "scenes/transactions";
+
+function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="products" element={<Products />} />
+              <Route path="customers" element={<Customers />} />
+              <Route path="/transactions" element={<Transactions />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ docs/Dashboard/projectInfo.js
@@ -0,0 +1,13 @@
+const dashboard = {
+  id: 1,
+  title: "Dashboard",
+  category: "TUTORIAL",
+  img: "./images/dashboard.PNG",
+  description:
+    "This is a part of a React/Redux MERN tutorial which I am currently working on. Not my project, for presentational purposes only.",
+  link: "./OWN LOREM IPSUM/index.html",
+  githubLink:
+    "https://github.com/zyjaruWeb/portfolio/tree/main/docs/OWN%20LOREM%20IPSUM",
+};
+
+export { dashboard };
