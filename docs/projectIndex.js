87489ce965a064fbfc4cc441a8af5bac2456edf4
zyjaru <128094313+zyjaruWeb@users.noreply.github.com>
2023-07-25 21:07:31 +0100

    Add files via upload

--- docs/projectIndex.js
+++ docs/projectIndex.js
@@ -1,18 +1,18 @@
 //create index for all projects
 
-const projects = []
+const projects = [];
 
-import {ownLoremIpsum} from "./OWN LOREM IPSUM/projectInfo.js"
-import {colorFlipper} from "./COLOR FLIPPER/projectInfo.js"
-import {simpleLedger} from "./simpleLedgerAPI/projectInfo.js"
+import { ownLoremIpsum } from "./OWN LOREM IPSUM/projectInfo.js";
+import { colorFlipper } from "./COLOR FLIPPER/projectInfo.js";
+import { simpleLedger } from "./simpleLedgerAPI/projectInfo.js";
+import { dashboard } from "./dashboardGit/projectInfo.js";
 
 // const {ownLoremIpsum} = ("../OWN LOREM IPSUM/projectInfo")
 // const {colorFlipper} = ("../COLOR FLIPPER/projectInfo")
 
-
 //connect different projects information into one array
-projects.push(ownLoremIpsum,colorFlipper, simpleLedger)
+projects.push(ownLoremIpsum, colorFlipper, simpleLedger, dashboard);
 
 // projects = [ownLoremIpsum, colorFlipper]
 
-export {projects}
+export { projects };
