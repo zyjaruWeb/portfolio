6121666ba869e18a44c3f882010f4fe78499d894
zyjaru <128094313+zyjaruWeb@users.noreply.github.com>
2023-07-26 00:45:19 +0100

    Add files via upload

--- docs/projectIndex.js
+++ docs/projectIndex.js
@@ -5,7 +5,7 @@ const projects = [];
 import { ownLoremIpsum } from "./OWN LOREM IPSUM/projectInfo.js";
 import { colorFlipper } from "./COLOR FLIPPER/projectInfo.js";
 import { simpleLedger } from "./simpleLedgerAPI/projectInfo.js";
-import { dashboard } from "./dashboard/projectInfo.js";
+import { dashboard } from "./Dashboard/projectInfo.js";
 
 // const {ownLoremIpsum} = ("../OWN LOREM IPSUM/projectInfo")
 // const {colorFlipper} = ("../COLOR FLIPPER/projectInfo")
